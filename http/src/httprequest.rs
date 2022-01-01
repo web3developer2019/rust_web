b02cf6cc5d8c59e4065f26efe126851537d00b5b
bookstoreTest <96944594+web3developer2019@users.noreply.github.com>
2022-01-01 22:09:11 +0800

    通过测试

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, os::windows::process};
+use std::collections::HashMap;
 
 #[derive(Debug,PartialEq)]
 pub enum Method{
@@ -43,7 +43,7 @@ pub struct  HttpRequest{
     pub method:Method,
     pub version:Version,
     pub resource:Resource,
-    pub header:HashMap<String,String>,
+    pub headers:HashMap<String,String>,
     pub msg_body:String,
 }
 /**
@@ -58,18 +58,18 @@ impl From<String> for HttpRequest {
         let mut parsed_msg_body="";
         for line in req.lines(){
             if line.contains("HTTP"){
-                let (method,resource,version) = process_req_line(req);
+                let (method,resource,version) = process_req_line(line);
                 parsed_method = method;
                 parsed_resource = resource;
                 parsed_version=version;
             }else if line.contains(":") { //处理header行
-                let (key,value) = process_header_line(req);
+                let (key,value) = process_header_line(line);
                 parsed_headers.insert(key,value);
                 
             }else if line.len()==0 {//处理空行
                 
             }else {//处理消息体
-                parsed_msg_body = process_msg_body(line);
+                parsed_msg_body = line;
             }
         }
         //将得到的信息组成HttpRequest类型返回
@@ -77,24 +77,23 @@ impl From<String> for HttpRequest {
             method:parsed_method,
             version:parsed_version,
             resource:parsed_resource,
-            header:parsed_headers,
+            headers:parsed_headers,
             msg_body:parsed_msg_body.to_string(),
         }
     }
 }
-//实现处理请求头函数
 fn process_req_line(s:&str)->(Method,Resource,Version){
     //将传入字符串按空白分成多个单词
     let mut words = s.split_whitespace();
     let  method = words.next().unwrap();
-    let mut resource = words.next().unwrap();
-    let mut version = words.next().unwrap();
+    let resource = words.next().unwrap();
+    let version = words.next().unwrap();
     (method.into(),Resource::Path(resource.to_string()),version.into(),)
 }
-
+//实现处理请求头函数
 fn process_header_line(s:&str)->(String,String){
     let mut header_items = s.split(":");
-    let mut key = String::from(""_);
+    let mut key = String::from("");
     let mut value = String::from("");
     if let Some(k) = header_items.next() {
         key = k.to_string();
@@ -105,11 +104,10 @@ fn process_header_line(s:&str)->(String,String){
     (key,value)
 }
 
-fn process_msg_body(s:&str)->&str{
 
-}
 #[cfg(test)]
 mod tests{
+
     use super::*;
     
     #[test]
@@ -122,5 +120,21 @@ mod tests{
     fn test_ver_into(){
         let m:Version = "HTTP/1.1".into();
         assert_eq!(m,Version::V1_1);
+    }
+
+    #[test]
+    fn test_http_request(){
+        let s:String = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.71.1\r\nAccept: */*\r\n\r\n");
+        //测试结果
+        let mut headers_expeced=HashMap::new();
+        headers_expeced.insert("Host".into(), " localhost".into());
+        headers_expeced.insert("Accept".into()," */*".into());
+        headers_expeced.insert("User-Agent".into(), " curl/7.71.1".into());
+        let req:HttpRequest=s.into();
+        
+        assert_eq!(Method::Get,req.method);
+        assert_eq!(Version::V1_1,req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()),req.resource);
+        assert_eq!(headers_expeced,req.headers);
     }
 }
